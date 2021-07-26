c8dd5f7b895e0277bf67c70a67f993ef2e2f3b3e
Dodgemaster11 <skrinnikv@gmail.com>
2021-07-26 22:10:32 +0300

    All functionality and animation restore

--- MemorizeGame/Cardify.swift
+++ MemorizeGame/Cardify.swift
@@ -2,22 +2,32 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
+struct Cardify: AnimatableModifier {
     
-    var isFaceUp: Bool 
+    init(isFaceUp: Bool ){
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get{ rotation }
+        set{ rotation = newValue }
+    }
+    
+    var rotation: Double // in degrees
     
     func body(content: Content) -> some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: DrawingConstans.cornerRadius)
-            if isFaceUp {
+            if rotation < 90 {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstans.lineWidth)
             } else {
                 shape.fill()
             }
             content
-                .opacity(isFaceUp ? 1 : 0)
+                .opacity(rotation < 90 ? 1 : 0)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
     
     private struct DrawingConstans {
--- MemorizeGame/MemoryGame.swift
+++ MemorizeGame/MemoryGame.swift
@@ -27,6 +27,10 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent){
         cards = []
         
@@ -35,15 +39,78 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
+        cards.shuffle()
     }
     
-    struct Card: Identifiable {
-        var isFaceUp = false
-        var isMatched = false
-        let content: CardContent
-        var id: Int
+    
+
+
+
+        struct Card: Identifiable {
+            var isFaceUp = false {
+                didSet {
+                    if isFaceUp {
+                        startUsingBonusTime()
+                    } else {
+                        stopUsingBonusTime()
+                    }
+                }
+            }
+            var isMatched = false {
+                didSet {
+                    stopUsingBonusTime()
+                }
+            }
+            let content: CardContent
+            var id: Int
+   
+//MARK! - Bonus Time
+
+var bonusTimeLimit: TimeInterval = 6
+
+private var faceUpTime: TimeInterval {
+    if let lastFaceUpDate = self.lastFaceUpDate {
+        return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+    } else {
+        return pastFaceUpTime
     }
 }
+
+var lastFaceUpDate: Date?
+
+var pastFaceUpTime: TimeInterval = 0
+
+
+var bonusTimeRemaining: TimeInterval {
+    max(0, bonusTimeLimit - faceUpTime)
+}
+
+var bonusRemaining: Double {
+    (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+}
+
+var hasEarnedBonus: Bool {
+    isMatched && bonusTimeRemaining > 0
+}
+
+var isConsumingBonusTime: Bool {
+    isFaceUp && !isMatched && bonusTimeRemaining > 0
+}
+
+private mutating func startUsingBonusTime() {
+    if isConsumingBonusTime, lastFaceUpDate == nil {
+        lastFaceUpDate = Date()
+    }
+}
+
+private mutating func stopUsingBonusTime() {
+    pastFaceUpTime = faceUpTime
+    self.lastFaceUpDate = nil
+}
+
+    }
+}
+
 
 extension Array {
     var oneAndOnly: Element? {
